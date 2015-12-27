0f0c9cb26361791c8d6fc7ed3b4f9c892ee8c605
Alex Angelini <alex.louis.angelini@gmail.com>
2015-12-27 16:02:27 -0500

    Added CSV to DB converter

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,55 @@
 extern crate regex;
 extern crate rustc_serialize;
 extern crate bincode;
+extern crate csv;
 
 mod data;
 mod ast;
 mod cli;
 
-use data::DB;
+use data::{DB, Datum};
 use ast::ASTNode;
 use cli::CLICommand;
 
+fn robotize(string: &str) -> String {
+    string.replace(" ", "_")
+          .to_lowercase()
+}
+
+fn convert_csv(entity: &str, filename: &str, time: &str) -> DB {
+    let mut rdr = csv::Reader::from_file(filename).unwrap();
+    let headers = rdr.headers().expect("Headers required to convert CSV");
+
+    let time_index = match headers.iter().enumerate().find(|&(_, h)| h == time) {
+        Some((idx, _)) => idx,
+        None => panic!("Time header not found {}", time),
+    };
+
+    let mut eid = 0;
+    let datums = rdr.records()
+                    .flat_map(|row_res| {
+                        let row = row_res.unwrap();
+                        let time_val = row[time_index].parse::<u32>().unwrap();
+                        eid += 1;
+                        headers.iter()
+                               .zip(row)
+                               .map(|(header, val)| {
+                                   Datum::new(eid,
+                                              format!("{}/{}", entity, robotize(header)),
+                                              val,
+                                              time_val)
+                               })
+                               .collect::<Vec<Datum>>()
+                    })
+                    .collect::<Vec<Datum>>();
+
+    DB::new(datums)
+}
+
 fn main() {
-    let mut db = DB::from_vec(vec![]);
-    println!("db {:?}", db);
+    let mut db = convert_csv("gdp", "gdp.csv", "Year");
+    // let mut db = DB::from_vec(vec![]);
+    println!("db {:?}", db.datums.iter().take(5).collect::<Vec<&Datum>>());
 
     loop {
         match cli::read() {
